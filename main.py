361585ecc32d009f4441d1414980afc001216ce7
wilodietch <wilodietch@gmail.com>
2020-09-11 23:59:07 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+# Project Planning
+# 2 players: player vs. computer
+# Ask player their choice
+# Computer makes choice
+# Outputting and comparing who won
+# players either choose: rock, paper or scissor
+# rock beats scissor
+# paper beats rock
+# scissor beats paper
+
+print ("Welcome to Rock, Paper, Scissors Showdown")
