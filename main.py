ea2b7ccf588c4eb78e9189ac80c8131c38d12f7d
wilodietch <wilodietch@gmail.com>
2020-09-12 00:13:11 +0000

    choices inputted

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import random
 # Project Planning
 # 2 players: player vs. computer
 # Ask player their choice
@@ -9,3 +10,13 @@
 # scissor beats paper
 
 print ("Welcome to Rock, Paper, Scissors Showdown")
+
+choices = ["rock", "paper", "scissors"]
+
+player_choice = input("Choose rock, paper, or scissors: \n")
+
+while player_choice not in choices:
+    player_choice = input("Not valid. Please choose rock, paper, or scissors \n")
+
+print(f"Player choice: {player_choice}")
+
